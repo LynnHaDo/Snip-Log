732013247fd713bc146f48faa9082df914efeeb1
Lynn Do <do24l@mtholyoke.edu>
2026-01-19 17:29:19 +0700

    Add snippet query functions

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,35 +1,101 @@
 import { v } from "convex/values";
-import { mutation } from './_generated/server';
-import { Id } from "./_generated/dataModel";
+import { mutation, query } from "./_generated/server";
 
 export const createSnippet = mutation({
-    args: {
-        title: v.string(),
-        language: v.string(),
-        code: v.string()
-    },
-    handler: async(context, args) => {
-        const identity = await context.auth.getUserIdentity()
+  args: {
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+  },
+  handler: async (context, args) => {
+    const identity = await context.auth.getUserIdentity();
 
-        if (!identity) {
-            throw new Error("User is not authenticated")
-        }
-
-        const user = await context.db.query("users")
-                               .withIndex("by_user_id")
-                               .filter(q => q.eq(q.field("userId"), identity.subject))
-                               .first()
-        
-        if (!user) {
-            throw new Error("User not found")
-        }
-
-        const snippetId = await context.db.insert("snippets", {
-            ...args,
-            userId: user._id,
-            userName: user.name
-        })
-
-        return snippetId
+    if (!identity) {
+      throw new Error("User is not authenticated");
     }
-})
+
+    const user = await context.db
+      .query("users")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), identity.subject))
+      .first();
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const snippetId = await context.db.insert("snippets", {
+      ...args,
+      userId: user._id,
+      userName: user.name,
+    });
+
+    return snippetId;
+  },
+});
+
+export const getSnippets = query({
+  handler: async (context) => {
+    const snippets = await context.db.query("snippets").order("desc").collect();
+    return snippets;
+  },
+});
+
+export const isSnippetStarred = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (context, args) => {
+    const identity = await context.auth.getUserIdentity();
+
+    if (!identity) {
+      return false;
+    }
+
+    const star = await context.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId),
+      )
+      .first();
+    return !!star;
+  },
+});
+
+export const getSnippetStarCount = query({
+  args: {
+    snippetId: v.id("snippets"),
+  },
+  handler: async (context, args) => {
+    const stars = await context.db
+      .query("stars")
+      .withIndex("by_snippet_id")
+      .filter((q) => q.eq(q.field("snippetId"), args.snippetId))
+      .collect();
+    return stars.length;
+  },
+});
+
+export const getStarredSnippets = query({
+  handler: async (context) => {
+    const identity = await context.auth.getUserIdentity();
+    if (!identity) {
+      return false;
+    }
+
+    const stars = await context.db
+      .query("stars")
+      .withIndex("by_user_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject)
+      )
+      .collect();
+    
+    const snippets = await Promise.all(stars.map(star => context.db.get(star.snippetId)))
+    return snippets.filter(snippet => !!snippet)
+  },
+});
